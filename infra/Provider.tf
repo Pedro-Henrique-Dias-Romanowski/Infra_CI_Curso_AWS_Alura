5ad0742866d5a1d2ed56b6b6516b292571b4c9f2
Pedro Henrique Dias Romanowksi <pedro.romanowski02@gmail.com>
2026-06-16 20:58:22 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=4.6.0"
+      version = ">= 4.47"
     }
   }
 
@@ -11,4 +11,30 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+}
+
+resource "kubernetes_service" "LoadBalancer" {
+  metadata {
+    name = "load-balancer-go-api"
+  }
+  spec {
+    selector = {
+      nome = "go"
+    }
+    port {
+      port = 8000
+      target_port = 8000
+    }
+    type = "LoadBalancer"
+  }
 }
